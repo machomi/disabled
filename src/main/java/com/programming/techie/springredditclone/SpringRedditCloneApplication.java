f53b15581a28e40fcbeeadf28eb595adbffb1629
Michal Machowski <machomi@users.noreply.github.com>
2020-12-13 11:30:35 +0100

    fixing problems with no entity manager factory bean

--- src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
+++ src/main/java/com/programming/techie/springredditclone/SpringRedditCloneApplication.java
@@ -2,10 +2,14 @@ package com.programming.techie.springredditclone;
 
 import com.programming.techie.springredditclone.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+//@EnableJpaRepositories("com.programming.techie.springredditclone.repository")
+//@EnableAutoConfiguration
 @SpringBootApplication
 @EnableAsync
 @Import(SwaggerConfiguration.class)
